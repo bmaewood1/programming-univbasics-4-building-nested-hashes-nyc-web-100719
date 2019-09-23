8e4b979b498fc50929e10cbaa7d96aaab8ac03b7
Bailey Wood <woodbmae@gmail.com>
2019-09-23 16:43:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,24 +33,4 @@ def monopoly_with_fourth_tier
   # complexity.
 end
 
-  describe "the Hash pointed to by :rent_in_dollars" do
-        it "has a key :one_piece_owned that points to a fee of 25" do
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars].has_key?(:one_piece_owned)).to(be_truthy, "key :one_piece_owned should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars][:one_piece_owned]).to(eq(25), ":one_piece_owned should point to the value 25")
-        end
 
-        it "has a key :two_pieces_owned that points to a fee of 50" do
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars].has_key?(:two_pieces_owned)).to(be_truthy, "key :two_pieces_owned should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars][:two_pieces_owned]).to(eq(50), ":two_pieces_owned should point to the value 50")
-        end
-
-        it "has a key :three_pieces_owned that points to a fee of 100" do
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars].has_key?(:three_pieces_owned)).to(be_truthy, "key :three_pieces_owned should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars][:three_pieces_owned]).to(eq(100), ":three_pieces_owned should point to the value 100")
-        end
-
-        it "has a key :four_pieces_owned that points to a fee of 200" do
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars].has_key?(:four_pieces_owned)).to(be_truthy, "key :four_pieces_owned should be present in this Hash")
-          expect(monopoly_with_third_tier[:railroads][:rent_in_dollars][:four_pieces_owned]).to(eq(200), ":four_pieces_owned should point to the value 200")
-        end
-    end
